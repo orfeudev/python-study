933fbb0224902a37712120aa9a0cad80e87fc81e
orfeudev <francgbsj1995@gmail.com>
2024-06-17 22:15:31 -0300

    initial commit

---
+++ stardust.py
@@ -0,0 +1,46 @@
+
+#funções py
+
+def funcao():
+    print("bloco de codigo")
+
+funcao()
+
+#parametros
+
+def saudacao():
+    print("Hello World!")
+
+saudacao()    
+
+def soma(a,b):
+    return a+b
+
+def resultado():
+    res = soma(3,4)
+    print(res)
+
+resultado()
+
+#F-string
+def nome(nome):
+    print(f"Nome: {nome}")
+
+nome("Joaozinho")
+
+#valores Default
+def flor(flor='rosa', cor='vermelha'):
+    print(f"A cor da {flor} é {cor}")
+
+flor()
+flor("Orquídea","Azul")
+
+#chamada de função posicional
+
+def monta_build(first_item='', sec_item=0, last_item=0):
+    print(f"A melhor build, em sequência, é \n\t - First-Item: {first_item}\n\t - Sec-Item: {sec_item} \n\t - Last_Item= {last_item}")
+
+monta_build('Black Cleaver', 1975, 1983)
+
+#função posicional respeita a ordem dos parâmetros
+
