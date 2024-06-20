809d336ede3c16907ac01f8379d05f270c857666
orfeudev <francgbsj1995@gmail.com>
2024-06-20 02:31:16 -0300

    statistics functions

---
+++ intfunc.py
@@ -0,0 +1,32 @@
+#importando a biblioteca statistics
+
+import statistics
+
+#funções internas de python
+
+#abs é uma função que passa o valor absoluto de um número(módulo)
+print(abs(-5))
+
+#retorna o maior item de um iterável ou o maior de dois ou mais argumentos
+print(max(1,2,3,4,121))
+
+#retorna o menor item de um iterável ou o menor de dois ou mais argumentos.
+print(min(-2,0,3,88))
+
+#arredonda o primeiro número para o numero de casas dado no segundo argumento.
+print(round(3.14159, 3))
+
+#soma todos os numeros
+print(sum([2,6,7,10,15]))
+
+#modulos statistics
+
+mean()
+median()
+mode()
+stdev()
+variance()
+
+#books.goalkicker.com/pythonbook
+#pythonfluente
+
