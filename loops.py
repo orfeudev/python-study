e42fb0293c7acc762bf9d28d14730421d78601ea
orfeudev <francgbsj1995@gmail.com>
2025-01-19 22:36:50 -0300

    estudos adicionais python

---
+++ loops.py
@@ -0,0 +1,10 @@
+
+# A iteração, no contexto da programação, é o processo de 
+# executar um bloco de código repetidamente para cada 
+# elemento de uma sequência de dados.
+
+
+frutas = ["maçã", "banana", "laranja"]
+
+for fruta in frutas:
+    print(f"Que delícia: {fruta}")
